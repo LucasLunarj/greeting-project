bff4a3d03c0eb974d58febda1fa7c697a892fe13
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-07-26 20:40:26 -0300

    new updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Page } from "./components/Page";
 function App() {
   return (
-    <div className="h-screen w-screen bg-blue-600">
+    <div className="h-screen w-screen bg-blue-600 flex justify-center items-center">
       <Page />
     </div>
   );
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,12 +4,25 @@ export const Page = () => {
     hour: "2-digit",
     minute: "2-digit",
   });
-  console.log(time);
+
+  const timeValue = parseFloat(time).toFixed(2);
+  let currentPeriod;
+  const currentMinute: number | string = current.getMinutes();
+  console.log(timeValue, time, current.getMinutes(), parseInt(currentMinute));
+
+  if (parseInt(time) >= 6 && parseInt(time) <= 12) {
+    currentPeriod = "Bom Dia";
+  } else if (parseInt(time) >= 12 && parseInt(time) <= 18) {
+    currentPeriod = "Boa Tarde";
+  } else {
+    currentPeriod = "Boa Noite";
+  }
 
   return (
-    <div className=" container mx-auto bg-slate-500">
+    <div className=" container mx-auto">
       <div>
-        <h1 className="text-8xl text-white">{time}</h1>
+        <h1 className="text-8xl text-white text-center ">{time}</h1>
+        <p className="text-white text-center text-xl">{currentPeriod}</p>
       </div>
     </div>
   );
